f6eb7fb853e2a774ce5f9c79fad30c729b052236
alejandrovr <alejandrovarelarial@gmail.com>
2020-04-01 18:28:05 +0200

    bug in tica

--- markov_model.py
+++ markov_model.py
@@ -13,10 +13,10 @@ from htmd.config import config
 from htmd.ui import *
 
 #for reference
-fragalysis = Molecule('noncovhits/Mpro-x0104.pdb')
+fragalysis = Molecule('/home/alejandro/covid19/noncovhits/Mpro-x0104.pdb')
 
 
-sims = simlist(glob('filtered/*/'), glob('filtered/filtered.pdb'))
+sims = simlist(glob('/shared/alejandro/corona/goal20/filtered/*/'), glob('/shared/alejandro/corona/goal20/filtered/filtered.pdb'))
 metr = Metric(sims)
 metr.set(MetricDistance('protein and noh' , 'resname MOL and noh', groupsel1='residue', groupsel2='residue', metric='contacts', threshold=5))
 data = metr.project()
@@ -25,7 +25,7 @@ data.fstep = 0.1
 data.plotTrajSizes()
 data.dropTraj()
 
-tica = TICA(data, 2, units='ns')
+tica = TICA(data, 1, units='ns')
 dataTica = tica.project(3)
 
 dataBoot = dataTica.bootstrap(0.8)
@@ -33,13 +33,13 @@ dataBoot.cluster(MiniBatchKMeans(n_clusters=1000))
 model = Model(dataBoot)
 model.plotTimescales()
 
-model.markovModel(5, 2, units='ns')
+model.markovModel(2, 5, units='ns')
 model.viewStates(ligand='resname MOL and noh')
 
 abc=model.getStates()
 prot_ref = abc[0].copy()
 prot_ref.filter('protein')
 frag_alig = sequenceStructureAlignment(fragalysis,prot_ref,molseg='', refseg='')[0]
-frag_alig.view(sel='resname MOL', style='Licorice')
+frag_alig.view(sel='resname LIG', style='Licorice')
 
 
